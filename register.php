59961e32e993d0b7cdf5bbc6234a8ce91a7f5298
study-hary-id <haryansyah.study.id@gmail.com>
2022-01-12 10:42:07 +0700

    Change location after successfully registered

--- register.php
+++ register.php
@@ -114,7 +114,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement.
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to homepage.
-                header("location: index.php");
+                header("location: login.php");
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
