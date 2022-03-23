dd48165fd2781735e693603dad7b74d62bfb8960
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-23 23:54:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,21 @@
 <?php
-/* Database credentials. Assuming you are running MySQL server with default setting. */
+
+/*
+    Database credentials.
+    Assuming you are running MySQL server with default setting.
+*/
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'phptest');
-define('DB_PASSWORD', '4Uth3nt1c4t10N_');
-define('DB_NAME', 'notes');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'school_payment_db');
 
 /* Attempt to connect to MySQL database. */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection, add connection handler.
+/* Check connection, add connection handler. */
 if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+// echo "Database connected succesfully.";
 ?>
