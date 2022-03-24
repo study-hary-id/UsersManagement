4a1bae58c44d22e6bdbfcf1316cd3cc3c2e10cbd
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-24 22:30:32 +0700

    Update UI and insert to database

--- register.php
+++ register.php
@@ -111,32 +111,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Check input errors before inserting in database.
-    // if(empty($email_err) && empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
-    //     // Prepare an insert statement.
-    //     $sql = "INSERT INTO users (email, username, password) VALUES (?, ?, ?)";
-         
-    //     if($stmt = mysqli_prepare($link, $sql)){
-    //         // Bind variables to the prepared statement as parameters.
-    //         mysqli_stmt_bind_param($stmt, "sss", $param_email, $param_username, $param_password);
-            
-    //         // Set parameters.
-    //         $param_email = $email;
-    //         $param_username = $username;
-    //         $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash.
-            
-    //         // Attempt to execute the prepared statement.
-    //         if(mysqli_stmt_execute($stmt)){
-    //             // Redirect to homepage.
-    //             header("location: login.php");
-    //         } else{
-    //             echo "Oops! Something went wrong. Please try again later.";
-    //         }
+    if (empty($email_err) && empty($username_err) && empty($password_err)
+        && empty($confirm_password_err) && empty($full_name_err)) {
 
-    //         // Close statement.
-    //         mysqli_stmt_close($stmt);
-    //     }
-    // }
+        // Prepare an insert statement.
+        $sql = "INSERT INTO users (
+                    email, username, password, full_name, address, phone_number
+                )
+                VALUES (?, ?, ?, ?, ?, ?)";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters.
+            mysqli_stmt_bind_param(
+                $stmt,
+                "ssssss",
+                $param_email,
+                $param_username,
+                $param_password,
+                $param_full_name,
+                $param_address,
+                $param_phone_number
+            );
+            
+            // Set parameters.
+            $param_email = $email;
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash.
+            $param_full_name = $full_name;
+            $param_address = $address;
+            $param_phone_number = $phone_number;
+            
+            // Attempt to execute the prepared statement.
+            if (mysqli_stmt_execute($stmt)) {
+                header("location: login.php");
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement.
+            mysqli_stmt_close($stmt);
+        }
+    }
 
     // Close connection.
     mysqli_close($link);
@@ -145,78 +160,93 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 require_once "header.php";
 ?>
 <body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    class="form-control
-                    <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>"
-                    value="<?php echo $email; ?>"
-                >
-                <span class="invalid-feedback"><?php echo $email_err; ?></span>
-            </div>
+    <div class="container">
+        <form
+            action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+            class="form-signin"
+            method="post"
+        >
+            <h1 class="h3 my-3 font-weight-normal text-center">
+                Sign Up | Register
+            </h1>
+            <p class="text-center">
+                Please fill this form to create an account.
+            </p>
 
-            <div class="form-group">
-                <label>Username</label>
-                <input
-                    type="text"
-                    name="username"
-                    class="form-control
-                    <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
-                    value="<?php echo $username; ?>"
-                >
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            <div class="form-row">
+                <div class="col-sm form-group">
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        class="form-control
+                        <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>"
+                        value="<?php echo $email; ?>"
+                    >
+                    <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                </div>
+
+                <div class="col-sm form-group">
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        class="form-control
+                        <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                        value="<?php echo $username; ?>"
+                    >
+                    <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                </div>
             </div>
   
-            <div class="form-group">
-                <label>Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    class="form-control
-                    <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
-                    value="<?php echo $password; ?>"
-                >
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            <div class="form-row">
+                <div class="col-sm form-group">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        class="form-control
+                        <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+                        value="<?php echo $password; ?>"
+                    >
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                </div>
+
+                <div class="col-sm form-group">
+                    <label>Confirm Password</label>
+                    <input
+                        type="password"
+                        name="confirm_password"
+                        class="form-control
+                        <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>"
+                        value="<?php echo $confirm_password; ?>"
+                    >
+                    <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                </div>
             </div>
 
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input
-                    type="password"
-                    name="confirm_password"
-                    class="form-control
-                    <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>"
-                    value="<?php echo $confirm_password; ?>"
-                >
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
+            <div class="form-row">
+                <div class="col-sm form-group">
+                    <label>Full Name</label>
+                    <input
+                        type="text"
+                        name="full-name"
+                        class="form-control
+                        <?php echo (!empty($full_name_err)) ? 'is-invalid' : ''; ?>"
+                        value="<?php echo $full_name; ?>"
+                    >
+                    <span class="invalid-feedback"><?php echo $full_name_err; ?></span>
+                </div>
 
-            <div class="form-group">
-                <label>Full Name</label>
-                <input
-                    type="text"
-                    name="full-name"
-                    class="form-control
-                    <?php echo (!empty($full_name_err)) ? 'is-invalid' : ''; ?>"
-                    value="<?php echo $full_name; ?>"
-                >
-                <span class="invalid-feedback"><?php echo $full_name_err; ?></span>
-            </div>
-
-            <div class="form-group">
-                <label>Phone Number</label>
-                <input
-                    type="text"
-                    name="phone-number"
-                    class="form-control"
-                    value="<?php echo $phone_number; ?>"
-                >
+                <div class="col-sm form-group">
+                    <label>Phone Number</label>
+                    <input
+                        type="text"
+                        name="phone-number"
+                        class="form-control"
+                        value="<?php echo $phone_number; ?>"
+                    >
+                </div>
             </div>
 
             <div class="form-group">
@@ -228,10 +258,12 @@ require_once "header.php";
             </div>
 
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+                <input type="submit" class="btn btn-primary px-4" value="Submit">
+                <!-- <input type="reset" class="btn btn-secondary ml-2" value="Reset"> -->
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+            <p class="text-center">
+                Already have an account? <a href="login.php">Login here</a>.
+            </p>
         </form>
     </div>    
 </body>
