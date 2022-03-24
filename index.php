e901de590259c227551d9645df9bad4d117517a8
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-25 00:11:06 +0700

    Update UI on index.php / homepage

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php echo "Hello, World!" ?>
+<?php
+
+// Initialize the session.
+session_start();
+
+require_once "header.php"
+?>
+<body>
+    <div class="welcome container text-center">
+        <h1 class="my-5">
+            Hi, there. Welcome to our site.
+        </h1>
+
+        <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) : ?>
+        <div class="row" style="gap: 1rem">
+            <a href="login.php" class="col-sm btn btn-primary">
+                Login
+            </a>
+            <a href="register.php" class="col-sm btn btn-link">
+                Register
+            </a>
+        </div>
+        <?php endif ?>
+    </div>
+</body>
+</html>
