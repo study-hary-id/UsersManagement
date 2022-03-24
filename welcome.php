c4a3e165a9239e5efa414c85856b93129f6bbb89
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-24 23:28:38 +0700

    Update UI and refactor welcome page

--- welcome.php
+++ welcome.php
@@ -1,29 +1,29 @@
 <?php
+
 // Initialize the session.
 session_start();
  
-// Check if the user is logged in, if not then redirect him to login page.
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+// Check if the user is logged in, if not then redirect to login page.
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
-</head>
+require_once "header.php";
+?>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
+    <div class="welcome container text-center">
+        <h1 class="my-5">
+            Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.
+        </h1>
+        <div class="row" style="gap: 1rem">
+            <a href="reset-password.php" class="col-sm btn btn-warning">
+                Reset Your Password
+            </a>
+            <a href="logout.php" class="col-sm btn btn-danger">
+                Sign Out of Your Account
+            </a>
+        </div>
+    </div>
 </body>
 </html>
