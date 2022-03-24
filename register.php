8cae3b427f44a40a93e91ef5776196c2f3115fc8
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-24 23:31:25 +0700

    Update register.php
    Check the session first to prevent access register page after logged in

--- register.php
+++ register.php
@@ -1,5 +1,14 @@
 <?php
 
+// Initialize the session.
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page.
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: welcome.php");
+    exit;
+}
+
 // Include config file.
 require_once "config.php";
  
