f2813d553ca8069a02de1241d1c65c7cee5fcbd3
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-25 22:23:49 +0700

    Add font awesome and more javascript

--- header.php
+++ header.php
@@ -10,9 +10,14 @@ $title = substr($title, 1, strpos($title, ".") - 1);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo ucfirst($title) ?> | School Payment</title>
+
     <link
         rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    >
+    <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
     >
     <style>
         body { background-color: #f5f5f5 }
@@ -42,4 +47,8 @@ $title = substr($title, 1, strpos($title, ".") - 1);
             }
         }
     </style>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
