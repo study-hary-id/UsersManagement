0ce016fc366abd7c6a17c5a17dd13bdda50308be
Muhammad Haryansyah <haryansyah.study.id@gmail.com>
2022-03-28 14:40:59 +0700

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ $title = substr($title, 1, strpos($title, ".") - 1);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo ucfirst($title) ?> | School Payment</title>
+    <title><?php echo ucfirst($title) ?> | Users Management</title>
 
     <link
         rel="stylesheet"
